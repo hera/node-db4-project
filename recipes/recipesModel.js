4eeee58c7fbc5032abfeeae2dff37b7cc0ad92ca
hera <a.heraimenka@gmail.com>
2020-07-16 14:24:04 -0700

    Get all ingredients for a given recipe id

--- recipes/recipesModel.js
+++ recipes/recipesModel.js
@@ -2,9 +2,22 @@ const db = require("../data/dbConfig");
 
 module.exports = {
     getRecipes,
+    getShoppingList
 };
 
 
+// Return all recipes
+
 function getRecipes () {
     return db("Recipe");
+}
+
+
+// Return all ingredients for a given recipe id
+
+function getShoppingList (recipeId) {
+    return db("RecipeIngredient")
+        .select("RecipeIngredient.Quantity", "Ingredient.Name")
+        .join("Ingredient", "RecipeIngredient.IngredientId", "=", "Ingredient.Id")
+        .where("RecipeIngredient.RecipeId", recipeId);
 }
--- recipes/recipesRouter.js
+++ recipes/recipesRouter.js
@@ -3,6 +3,9 @@ const recipeDb = require("./recipesModel");
 
 const router = express.Router();
 
+
+// Get all recipes
+
 router.get("/", (req, res) => {
     recipeDb.getRecipes()
         .then(recipes => {
@@ -15,5 +18,22 @@ router.get("/", (req, res) => {
             });
         });
 });
+
+
+// Get all ingredients for a given recipe id
+
+router.get("/:id/shoppinglist", (req, res) => {
+    recipeDb.getShoppingList(req.params.id)
+        .then(ingredients => {
+            res.status(200).json(ingredients);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Server error. Could not get all ingredients for the given recipe.",
+                description: error
+            });
+        });
+});
+
 
 module.exports = router;
